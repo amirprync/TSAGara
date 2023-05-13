f0509993c4b7ec80467e5d5390cee9db923a570f
amirprync <60889238+amirprync@users.noreply.github.com>
2023-05-12 22:00:51 -0300

    Update app.py

--- app.py
+++ app.py
@@ -60,7 +60,7 @@ st.sidebar.title("Archivo RECEPCION GRTIAS TSA")
 reinv2 = st.sidebar.file_uploader("Carga tu xlsx de reinversión", type=['xlsx'], key="gracias_tsa_reinv2_file")
 st.sidebar.markdown("---")
 
-st.sidebar.title("solo TEST para CNV")
+st.sidebar.title("Reinversion")
 TEST = st.sidebar.file_uploader("Carga tu xlsx del mes a controlar TEST", type=['xlsx'], key="test_file")
 st.sidebar.markdown("---")
 
@@ -762,46 +762,80 @@ def main():
                     ) 
     
     if TEST:
-        test_fondos = pd.read_excel(TEST, engine='openpyxl')
+        columnas = ['Comitente Número','Moneda','Importe']
+        tablero = pd.read_excel(reinv, usecols=columnas, engine='openpyxl')
+        tablero_xls = pd.read_excel(reinv,engine='openpyxl')
+        comit = tablero['Comitente Número']
+        # st.text(comit)
 
-        coor = st.file_uploader("Carga tu xlsx COORPORATIVO", type=['xls'])
-        pymes = st.file_uploader("Carga tu xlsx PYME", type=['xls'])
+        st.dataframe(tablero)
+        # st.table(tablero)
+    
+        ################################ EXCEL PREPARACION #############################
+        
+        def crearSheet(archivo):
+            archivo = archivo
+            # print(archivo)
 
-        if coor and pymes:
+            sheet = {'Fecha Concertacion':[],
+                      'Fecha Vencimiento':[],
+                      'Cuenta':[],
+                      'Concepto':[],
+                      'Debe':[],
+                      'Haber':[],
+                      'Contraparte - Custodia':[],
+                      'Contraparte - Depositante':[],
+                      'Contraparte - Cuenta':[]}
+    
+            for num in archivo.index:
+                # print(num)
+                
+                fecha = datetime.now()
+                fecha = fecha.strftime("%d/%m/%Y")
 
-            coor = pd.read_excel(coor)
-            pymes = pd.read_excel(pymes)
+                sheet['Fecha Concertacion'].append(fecha)         
+                sheet['Fecha Vencimiento'].append(fecha)         
+                sheet['Cuenta'].append(archivo['Comitente Número'][num])         
+                sheet['Concepto'].append(archivo['Tipo'][num])        
+                sheet['Debe'].append('0,00')         
+                sheet['Haber'].append(archivo['Importe'][num])
+                sheet['Contraparte - Custodia'].append('CAJAVAL')
+                sheet['Contraparte - Depositante'].append('0046')
+                sheet['Contraparte - Cuenta'].append(archivo['Comitente Número'][num])
 
-            # test_fondos['TIPO'] = test_fondos['Custodia']
-            tipo = []
-            # print(coor['Código de Interfaz'])
-            st.dataframe(coor['Código de Interfaz'])
+            sheet = pd.DataFrame(sheet)
+            return sheet            
 
-            for contador, e in enumerate(test_fondos['Cuenta - Nro']):
-                print(e)
+        moneda_7000 = tablero_xls['Moneda'] == 'Dolar Renta Exterior - 7.000' 
+        moneda_10000 = tablero_xls['Moneda'] == 'Dolar Renta Local - 10.000'
+        moneda_8000 = tablero_xls['Moneda'] == 'Pesos Renta - 8.000'
+        nuevo7000 = tablero_xls[moneda_7000]
+        nuevo10000 = tablero_xls[moneda_10000]
+        nuevo8000 = tablero_xls[moneda_8000]
 
+        reinversion_xls = nuevo7000.append(nuevo10000)
+        reinversion_xls = reinversion_xls.append(nuevo8000)
+      
+        reinversion_xls = reinversion_xls.reindex(columns=['Número','Comitente Descripción','Fecha','Moneda','Comitente Número',
+            'Importe','Tipo','Banco','Tipo de Cuenta','Sucursal','Cuenta','CBU','Tipo de identificador impositivo','Número de identificador impositivo',
+            'Titular','Estado'])
 
-                if e in coor['Número de Custodia'].values:
-                    tipo.append("COORPORATIVO")
+        sheet_7000 = crearSheet(nuevo7000.set_index('Número'))
+        sheet_10000 = crearSheet(nuevo10000.set_index('Número'))
+        sheet_8000 = crearSheet(nuevo8000.set_index('Número'))
 
-                elif e in pymes['Número de Custodia'].values:
-                    tipo.append("PYMES")
+        with ExcelWriter('REINVERSION_FECHA.xlsx') as writer:
+            reinversion_xls.to_excel(writer,sheet_name='Sheet1',index=False)
+            sheet_7000.to_excel(writer,sheet_name='7000',index=False)  
+            sheet_10000.to_excel(writer,sheet_name='10000',index=False)  
+            sheet_8000.to_excel(writer,sheet_name='8000',index=False)  
+        
+        control_file = 'REINVERSION_FECHA.xlsx'
+        with open(control_file, 'rb') as f:
+            s = f.read()
 
-                else:
-                    tipo.append("NADA")
-
-            test_fondos['TIPO'] = tipo
-            st.dataframe(test_fondos)
-            ###########   GUARDAMOS NUEVO EXCEL CON AMBAS SHEETS #######################
-            with ExcelWriter('DATOS_CNV.xlsx') as writer:
-                test_fondos.to_excel(writer,index=False) 
-            control_file = 'DATOS_CNV.xlsx'
-            with open(control_file, 'rb') as f:
-                s = f.read()
-
-            download_button_str = download_button(s, control_file, f'EXCEL LISTO {control_file}')
-            st.markdown(download_button_str, unsafe_allow_html=True)                 
-
+        download_button_str = download_button(s, control_file, f'EXCEL LISTO {control_file}')
+        st.markdown(download_button_str, unsafe_allow_html=True)
     if bo:
 
         columnas = ['Comitente - Descripción','Instrumento - Símbolo','Instrumento - Denominación','Cuenta - Nro','Saldo Total']
